b13738d0ff13c70f25dde56bd3ddd8861efc84b6
박철현 <tghy4@naver.com>
2024-03-05 21:42:06 +0900

    feat.ItemRepository 클래스 생성 및 주요 기능 구현

--- src/main/java/SpringLab/domain/item/Item.java
+++ src/main/java/SpringLab/domain/item/Item.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-
 public class Item {
     //id, 상품명, 가격, 수량
     //수량이나 가격이 null(값이 없는채로 등록될 수도 있기 때문에 Integer 타입으로 선언)
--- src/main/java/SpringLab/domain/item/ItemRepository.java
+++ src/main/java/SpringLab/domain/item/ItemRepository.java
@@ -3,13 +3,36 @@ package SpringLab.domain.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+
 
 @Repository
 public class ItemRepository {
     private static final Map<Long, Item> store = new ConcurrentHashMap<>();
-    /*private static AtomicLong sequence = new AtomicLong();*/
     private static Long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+    public void updateItem(Long id, Item item) {
+        Item findItem = findById(id);
+        findItem.setName(item.getName());
+        findItem.setPrice(item.getPrice());
+        findItem.setQuantity(item.getQuantity());
+    }
+    //테스트 시 사용하기 위한 메서드
+    public void clearStore() {
+        store.clear();
+    }
 }
