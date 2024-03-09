d1d195f5f80b48cfc8b143e968f72f74e4873ecd
박철현 <tghy4@naver.com>
2024-03-09 17:03:42 +0900

    스프링 컨테이너의 싱글톤 방식 사용시 주의할점

--- src/main/java/Lab/Singtone/StatefulService.java
+++ src/main/java/Lab/Singtone/StatefulService.java
@@ -5,7 +5,7 @@ public class StatefulService {
 
     public void order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price;
+        this.price = price; // -> 해당 코드조각이 문제가 된다. 내부에서 들어온 정보가 자신한테 할당 될 때 (stateful)
     }
 
     public int getPrice() {
