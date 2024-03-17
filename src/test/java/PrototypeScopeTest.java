53b3f0251553d9517d1cbf286b542f4f08568edd
박철현 <tghy4@naver.com>
2024-03-17 17:56:40 +0900

    prototype bean 요청 시 스프링 컨테이너가 반환하는 객체가 서로 다른지 테스트

--- src/test/java/PrototypeScopeTest.java
+++ src/test/java/PrototypeScopeTest.java
@@ -1,15 +1,38 @@
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrototypeScopeTest {
 
     @Test
     void prototypeBeanScopeTest() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PrototypeScope.class);
+
+        PrototypeScope bean1 = context.getBean(PrototypeScope.class);
+        PrototypeScope bean2 = context.getBean(PrototypeScope.class);
+
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+
+        assertThat(bean1).isNotSameAs(bean2);
+
 
     }
 
     @Scope("prototype")
     static class PrototypeScope {
 
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeScope.init");
+        }
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeScope.destroy");
+        }
     }
 }
