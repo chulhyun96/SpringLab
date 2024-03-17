b99fe70ad9f840110a3ad0c53df5c9fa3e0ae330
박철현 <tghy4@naver.com>
2024-03-17 17:47:54 +0900

    스프링 컨테이너가 @PostConstruct를 실행하고 항상 같은 객체를 반환하는지 테스트

---
+++ src/test/java/SingleToneScopeTest.java
@@ -0,0 +1,29 @@
+import jakarta.annotation.PostConstruct;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingleToneScopeTest {
+
+    @Test
+    void singleToneBeanTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(SingleToneBeanTest.class);
+        SingleToneBeanTest bean1 = ac.getBean(SingleToneBeanTest.class);
+        SingleToneBeanTest bean2 = ac.getBean(SingleToneBeanTest.class);
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+
+        Assertions.assertThat(bean1).isSameAs(bean2);
+    }
+
+    @Scope("singleton")
+    static class SingleToneBeanTest {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingToneBeanTest.init");
+        }
+    }
+}
