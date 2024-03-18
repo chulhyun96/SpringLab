f22fab15f961d4da43883836db7570a9040fed59
박철현 <tghy4@naver.com>
2024-03-19 07:23:02 +0900

    프로토타입 빈과싱글톤 빈이 함께 사용시 프로토타입 빈은 새로 생성되는 객체가 맞는가에 대한 테스트 코드

--- src/test/java/SingleToneWithTest1.java
+++ src/test/java/SingleToneWithTest1.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Scope;
 public class SingleToneWithTest1 {
 
     @Test
-    void singleToneWithPrototype() {
+    void singleToneWithPrototype1() {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PrototypeBean.class);
         PrototypeBean bean1 = context.getBean(PrototypeBean.class);
         bean1.addCount();
@@ -15,6 +15,36 @@ public class SingleToneWithTest1 {
         PrototypeBean bean2 = context.getBean(PrototypeBean.class);
         bean2.addCount();
         Assertions.assertThat(bean2.getCount()).isEqualTo(1);
+    }
+    @Test
+    void singleToneWithPrototype2() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
+        ClientBean bean1 = context.getBean(ClientBean.class);
+        int logic1 = bean1.logic();
+        Assertions.assertThat(logic1).isEqualTo(1);
+        PrototypeBean p1 = context.getBean(PrototypeBean.class);
+        PrototypeBean p2 = context.getBean(PrototypeBean.class);
+
+        ClientBean bean2 = context.getBean(ClientBean.class);
+        int logic2 = bean2.logic();
+        Assertions.assertThat(logic2).isEqualTo(2);
+
+        Assertions.assertThat(bean1).isSameAs(bean2);
+        Assertions.assertThat(p1).isNotSameAs(p2);
+    }
+    @Scope
+    static class ClientBean {
+
+        private final PrototypeBean prototypeBean;
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
     }
 
     @Scope("prototype")
